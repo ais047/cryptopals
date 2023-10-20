51bc39c30417a5a3cf08b8af5544d1a024b4f139
root <root@Seiu>
2023-10-20 14:56:49 -0400

    setup dir

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cryptopals
 Just a fun little repo to try to work on cryptopals challenge using rust
+
+We're using rust-script to simplify each solution to one script.
---
+++ set1/1.rs
@@ -0,0 +1,9 @@
+#!/usr/bin/env rust-script
+
+// Convert Hex to Base64
+// HEX: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+// Targetted Result: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+fn main() {
+    println!("Set 1 Challenge 1");
+}
